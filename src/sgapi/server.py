2639932f8317ea3a9924daa4245a1cd229662886
Ahmed a305 <a7med@x000001.ies.com>
2019-05-26 00:01:39 +0300

    sgpy-1.0.7

---
+++ src/sgapi/server.py
@@ -0,0 +1,7 @@
+import http.server
+import socketserver
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
